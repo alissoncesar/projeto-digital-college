3942e4e7a522aadcd235da03ecdb5efbf402481c
alissoncesar <alissoncesar1113@gmail.com>
2025-02-02 13:41:26 -0300

    finalizando telas para o celular

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
                 sans: ['Open Sans', 'sans-serif']
                 },
             colors: {
+                'branco':'#FFFFFF',
+                'pink': '#C81C6F',
+                'cinza-escuro':'#333333',
+                'cinza':'#464646',
+                'cinza-claro':'#555555',
+                'gray':'#242424'
             }
         },
     },
