4317a50b88c9773ead3a43aeffdb5918f13cfecf
alissoncesar <alissoncesar1113@gmail.com>
2025-02-02 23:36:33 -0300

    tela finalizada

---
+++ index.js
@@ -0,0 +1,15 @@
+function novoTeam() {
+    document.getElementById('criar-team').style.display = "block";
+}
+
+function fecharNovoTeam() {
+    document.getElementById('criar-team').style.display = "none";
+}
+
+function adicionarNome() {
+    document.getElementById('adicionar-participante').style.display = "block";
+}
+
+function fecharAdicionarNome() {
+    document.getElementById('adicionar-participante').style.display = "none";
+}
