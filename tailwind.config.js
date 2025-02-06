b00d967c60f74fac5e72108ac6159464f7a8a54f
alissoncesar <alissoncesar1113@gmail.com>
2025-02-06 06:31:19 -0300

    finalizado para entrega

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
     content: [],
     theme: {
         extend: {
+            screens: {
+                'xl': '1180px',
+                '2xl':'1456px',
+            },
             fontFamily: {
                 sans: ['Open Sans', 'sans-serif']
                 },
